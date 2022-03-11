8d968eeeed906465ec67a8f9a2a3ff1e1ec64ae4
mohammad <mr.dori.dev@gmail.com>
2022-03-11 13:04:12 +0330

    add todoapp models

---
+++ todoapp/models.py
@@ -0,0 +1,13 @@
+"""todoapp models"""
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class TodoItem(models.Model):
+    work = models.CharField("کار", max_length=90)
+    checked = models.BooleanField(default=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    date = models.CharField("زمان(اختیاری)", blank=True, null=True, max_length=50)
+
+    def __str__(self):
+        return self.work
