4115fb7c24925aa18486d9b44aa09bb09352e290
mohammad <mr.dori.dev@gmail.com>
2022-03-11 13:02:32 +0330

    write accounts views functions

---
+++ accounts/views.py
@@ -0,0 +1,43 @@
+"""views"""
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+
+
+def register_view(request):
+    """register view"""
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("/")
+    else:
+        form = UserCreationForm()
+    arg = {
+        "form": form,
+    }
+    return render(request, "accounts/register.html", arg)
+
+
+def login_view(request):
+    """login view"""
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("/")
+    else:
+        form = AuthenticationForm()
+    arg = {
+        "form": form,
+    }
+    return render(request, "accounts/login.html", arg)
+
+
+def logout_view(request):
+    """logout view"""
+    if request.method == "POST":
+        logout(request)
+        return redirect("/")
