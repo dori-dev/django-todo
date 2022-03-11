6ecabace7de07f464602cbd5d646bc5e21c61126
mohammad <mr.dori.dev@gmail.com>
2022-03-11 13:02:16 +0330

    add login, logout and register to url

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+"""urls"""
+from django.urls import path
+from .views import register_view, login_view, logout_view
+
+app_name = "accounts"
+urlpatterns = [
+    path("register/", register_view, name="register"),
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
+]
+
+# TODO check redirect 
